1a19279fff58236c6165d23b76fed975dd405f4f
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 12:41:04 -0400

    Update App.js
    Using the forecast api instead of weather to get 3 hour forecast.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,9 @@ class App extends React.Component{
     super(props);
 
     this.state = {
-      temperature: undefined,
       city: undefined,
       country: undefined,
-      humidity: undefined,
-      description: undefined,
+      weatherData : {},
       error: undefined
     };
   };
@@ -27,18 +25,20 @@ class App extends React.Component{
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Open_Weather_Map_Key}`);
+    // api call to get only a single point of weather data
+    // const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Open_Weather_Map_Key}`);
 
-    const response = await api_call.json();
+    // api call to get 5 day/ 3 hour forecast data
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${Open_Weather_Map_Key}`);
+
+    const response = await api_call.json();    
 
     // Don't set the state unless user enters both city and country
     if(city && country){
       this.setState({
-        temperature: response.main.temp,
-        city: response.name,
-        country: response.sys.country,
-        humidity: response.main.humidity,
-        description: response.weather[0].description,
+        city: response.city.name,
+        country: response.city.country,
+        weatherData: response,
         error: ""
       });
     }
@@ -58,11 +58,9 @@ class App extends React.Component{
         <Titles />
         <Form loadWeather={this.getWeather}/>
         <Weather 
-          temperature={this.state.temperature}
           city={this.state.city}
           country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
+          weatherData={this.state.weatherData}
           error={this.state.error}/>
       </div>
     );
