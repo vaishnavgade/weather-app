79c50bdd2375bc2e9bbc85c33df1ef383c7d1969
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 14:00:35 -0400

    Update weather.js
    Displaying the date time after parsing

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+/**
+ * Takes a date string and returns a <div> containing the Time and Day of the week
+ * @param {String<TYPE>} dateString 
+ */
+const renderTimeAndDay = (dateString) =>{
+  return(
+    <div>
+      <label>{new Intl.DateTimeFormat('en-US', {hour: 'numeric', minute: 'numeric'}).format(new Date(dateString))}</label>
+      <br />
+      <label>{new Intl.DateTimeFormat('en-US', {weekday: 'short'}).format(new Date(dateString))}</label>
+    </div>
+  );
+}
+
 const Weather = (props) =>{
   return(
     <div>
@@ -8,23 +22,27 @@ const Weather = (props) =>{
       {
         props.city && props.country && <h1>{props.city}, {props.country} Forecast</h1> &&
         <table>
-          <tr className="table-headers">
-            <th>TIME</th>
-            <th>CONDITION</th>
-            <th>TEMP</th>
-            <th>HUMIDITY</th>
-            <th>WIND</th>
-          </tr>
-          {/* TODO: Add a unique key to each row rendered */}
-          {props.weatherData.list.map(item => (
-            <tr className="table-rows">
-              <td>{item.dt_txt}</td>
-              <td>{item.weather[0].description}</td>
-              <td>{item.main.temp}</td>
-              <td>{item.main.humidity}</td>
-              <td>{item.wind.speed}</td>
+          <thead>
+            <tr className="table-headers">
+              <th>TIME</th>
+              <th>CONDITION</th>
+              <th>TEMP</th>
+              <th>HUMIDITY</th>
+              <th>WIND</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {props.weatherData.list.map((item, index) => (
+              <tr className="table-rows" key={index}>
+                {/* Converting the DateTime string into a weekday */}
+                <td>{renderTimeAndDay(item.dt_txt)}</td>
+                <td>{item.weather[0].description}</td>
+                <td>{item.main.temp}</td>
+                <td>{item.main.humidity}</td>
+                <td>{item.wind.speed}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       }
       {props.error && <p className="weather_error">{props.error}</p>}
