cf55adff8346d5ab6609157fd9d36adf891cb9df
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 10:32:00 -0400

    Update App.js
    Getting the city and country form data to make the api call dynamic

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends React.Component{
     // Prevents the full page refresh that occurs by default
     e.preventDefault();
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Open_Weather_Map_Key}`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Open_Weather_Map_Key}`);
 
     const response = await api_call.json();
 
