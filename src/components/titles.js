a65300844dcfebcb733703b4768d29ffc2882ffc
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 15:10:24 -0400

    Update titles.js
    Adding a unit selection for displaying temperature

--- src/components/titles.js
+++ src/components/titles.js
@@ -4,6 +4,10 @@ const Titles = (props) => {
   return(
     <div className="title-container">
       <h1>Weather App</h1>
+      <select onChange={props.getTempUnits}>
+        <option value="c">°C</option>
+        <option value="f">°F</option>
+      </select>
     </div>
   );
 };
