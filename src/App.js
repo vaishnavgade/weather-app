944dc38b8c7303619782bb3dd2d034717ba64839
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 10:10:37 -0400

    Update App.js
    Adding the components created to display in the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Titles from './components/titles';
+import Form from './components/form';
+import Weather from './components/weather';
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        // add components here
+        {/* Add Components here */}
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   };
 }
+
+export default App;
