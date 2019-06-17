7d72181d7bd19c1dfaf54c69e33da00dfdc2060a
Sri Vaishnav Gade <vaishnav.gade@gmail.com>
2019-06-17 10:28:17 -0400

    Update App.js
    Adding an async method to make an api call to get weather data.
    
    Creating a new prop on the Form component to pass the api call.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import Titles from './components/titles';
 import Form from './components/form';
 import Weather from './components/weather';
+import { async } from 'q';
+
 
 class App extends React.Component{
+
+  getWeather = async(e) =>{
+    // Prevents the full page refresh that occurs by default
+    e.preventDefault();
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Open_Weather_Map_Key}`);
+
+    const response = await api_call.json();
+
+    console.log(response);
+  };
+
   render(){
     return(
       <div>
         {/* Add Components here */}
         <Titles />
-        <Form />
+        <Form loadWeather={this.getWeather}/>
         <Weather />
       </div>
     );
